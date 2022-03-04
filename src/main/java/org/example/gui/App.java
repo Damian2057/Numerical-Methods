25e8d7dce2fe0c29f2616a2cd7f2a5ba408eba44
UN5TOPP4BLE <mateuszdangreaux@gmail.com>
2022-03-04 17:45:04 +0100

    dodanie wprowadzania danych w app.java

--- src/main/java/org/example/gui/App.java
+++ src/main/java/org/example/gui/App.java
@@ -2,6 +2,7 @@ package org.example.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import java.util.Scanner;
 import org.example.model.Bisection;
 
 public class App extends Application {
@@ -9,11 +10,26 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Bisection bisection = new Bisection("xd");
-        System.out.println(bisection.bisectionAlgorithm(0,2,0.0001));
+
+        double lowerLimit , upperLimit, dokladnosc;
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Podaj dolny przedzial: ");
+        lowerLimit = sc.nextDouble();
+        System.out.print("Podaj gorny przedzial: ");
+        upperLimit = sc.nextDouble();
+        System.out.print("Podaj dokladnosc: ");
+        dokladnosc = sc.nextDouble();
+
+        System.out.println(bisection.bisectionAlgorithm(lowerLimit,upperLimit,dokladnosc));
+//        System.out.println(bisection.bisectionAlgorithm(0,2,0.0001));
         System.out.println("test");
     }
 
     public static void main(String[] args) {
         launch();
+
+
+
     }
 }
