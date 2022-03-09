1eb3268bc6b13fea80b7db64425c323de7f1b34c
UN5TOPP4BLE <mateuszdangreaux@gmail.com>
2022-03-09 15:13:56 +0100

    przekomentowanie appa i stworzenie klasy Trigonometry

--- src/main/java/org/example/gui/App.java
+++ src/main/java/org/example/gui/App.java
@@ -15,74 +15,93 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-
-
-        System.out.println("Podaj wzor funkcji np. sin(x+1): ");
         Scanner scanner= new Scanner(System.in);
-        String wzor = scanner.nextLine();
 
-        //podajemy dolny i gorny przedzial oraz liczbe iteracji/dokladnosc
-        double lowerLimit , upperLimit, stop;
-        System.out.print("Podaj dolny przedzial: ");
-        lowerLimit = scanner.nextDouble();
-        System.out.print("Podaj gorny przedzial: ");
-        upperLimit = scanner.nextDouble();
-        System.out.println("Podaj warunek stopu (liczba >=1 to liczba iteracji, <1 to dokladnosc): ");
-        stop = scanner.nextDouble();
+        System.out.println("Podaj pierwszy typ funkcji (tryg/wiel/wyk): ");
+        String wybor1 = scanner.nextLine();
 
-        Bisection bisection = new Bisection(wzor);
-        Falsi falsi = new Falsi(wzor);
-        double bisectionZero = 0;
-        double falsiZero = 0;
-        try {
-            if(stop >=1) {
-                int stop2 = (int) stop;
-                bisectionZero = bisection.bisectionAlgorithm(lowerLimit,upperLimit,stop2);
-                System.out.println("B "+ bisectionZero);
-                falsiZero = falsi.falsiAlgorithm(lowerLimit,upperLimit,stop2);
-                System.out.println("F "+ falsiZero);
-            } else {
-                bisectionZero = bisection.bisectionAlgorithmE(lowerLimit, upperLimit, stop);
-                System.out.println("B "+ bisectionZero);
-                falsiZero = falsi.falsiAlgorithmE(lowerLimit, upperLimit, stop);
-                System.out.println("F "+ falsiZero);
+        switch (wybor1) {
+            case "tryg"-> {
+
             }
-        } catch (NoZeroException e) {
-            System.out.println("Funkcja wydaje sie nie miec miejsca zerowego na podanym przedziale");
-        } catch (Exception e) {
-            System.out.println("Wykryto blad");
+            case "wyk"-> {
+                System.out.println("2");
+            }
+            case "wiel"-> {
+                System.out.println("3");
+            }
+            default-> {
+                System.out.println("Blad wprowadzania danych!");
+                Platform.exit();
+            }
         }
 
-        double index = 0;
-        double value = 0;
+//        System.out.println("Podaj wzor funkcji np. sin(x+1): ");
+//        Scanner scanner= new Scanner(System.in);
+//        String wzor = scanner.nextLine();
+//
+//        //podajemy dolny i gorny przedzial oraz liczbe iteracji/dokladnosc
+//        double lowerLimit , upperLimit, stop;
+//        System.out.print("Podaj dolny przedzial: ");
+//        lowerLimit = scanner.nextDouble();
+//        System.out.print("Podaj gorny przedzial: ");
+//        upperLimit = scanner.nextDouble();
+//        System.out.println("Podaj warunek stopu (liczba >=1 to liczba iteracji, <1 to dokladnosc): ");
+//        stop = scanner.nextDouble();
+//
+//        Bisection bisection = new Bisection(wzor);
+//        Falsi falsi = new Falsi(wzor);
+//        double bisectionZero = 0;
+//        double falsiZero = 0;
+//        try {
+//            if(stop >=1) {
+//                int stop2 = (int) stop;
+//                bisectionZero = bisection.bisectionAlgorithm(lowerLimit,upperLimit,stop2);
+//                System.out.println("B "+ bisectionZero);
+//                falsiZero = falsi.falsiAlgorithm(lowerLimit,upperLimit,stop2);
+//                System.out.println("F "+ falsiZero);
+//            } else {
+//                bisectionZero = bisection.bisectionAlgorithmE(lowerLimit, upperLimit, stop);
+//                System.out.println("B "+ bisectionZero);
+//                falsiZero = falsi.falsiAlgorithmE(lowerLimit, upperLimit, stop);
+//                System.out.println("F "+ falsiZero);
+//            }
+//        } catch (NoZeroException e) {
+//            System.out.println("Funkcja wydaje sie nie miec miejsca zerowego na podanym przedziale");
+//        } catch (Exception e) {
+//            System.out.println("Wykryto blad");
+//        }
+//
+//        double index = 0;
+//        double value = 0;
+//
+//        if(lowerLimit < upperLimit) {
+//            index = lowerLimit;
+//            value = upperLimit;
+//        } else {
+//            index = upperLimit;
+//            value = lowerLimit;
+//        }
+//
+//        ArrayList<Double> x = new ArrayList<>();
+//        ArrayList<Double> y = new ArrayList<>();
+//
+//        while(index <= value) {
+//            x.add(index);
+//            y.add(bisection.f(index));
+//            index+=Math.abs((upperLimit-lowerLimit)*(1.0/300));
+//        }
 
-        if(lowerLimit < upperLimit) {
-            index = lowerLimit;
-            value = upperLimit;
-        } else {
-            index = upperLimit;
-            value = lowerLimit;
-        }
 
-        ArrayList<Double> x = new ArrayList<>();
-        ArrayList<Double> y = new ArrayList<>();
-
-        while(index <= value) {
-            x.add(index);
-            y.add(bisection.f(index));
-            index+=Math.abs((upperLimit-lowerLimit)*(1.0/300));
-        }
-
-
-        //Bisection graph
-        final XYSeriesDemo demoBisection = new XYSeriesDemo("Bisekcja",x,y, bisectionZero);
-        demoBisection.pack();
-        demoBisection.setVisible(true);
-
-        //Falsi graph
-        final XYSeriesDemo demoFalsi = new XYSeriesDemo("Falsi",x,y, falsiZero);
-        demoFalsi.pack();
-        demoFalsi.setVisible(true);
+//        //Bisection graph
+//        final XYSeriesDemo demoBisection = new XYSeriesDemo("Bisekcja",x,y, bisectionZero);
+//        demoBisection.pack();
+//        demoBisection.setVisible(true);
+//
+//        //Falsi graph
+//        final XYSeriesDemo demoFalsi = new XYSeriesDemo("Falsi",x,y, falsiZero);
+//        demoFalsi.pack();
+//        demoFalsi.setVisible(true);
 
         Platform.exit();
     }
@@ -91,3 +110,5 @@ public class App extends Application {
         launch();
     }
 }
+
+
---
+++ src/main/java/org/example/model/Trigonometry.java
@@ -0,0 +1,24 @@
+package org.example.model;
+
+
+import java.util.Scanner;
+
+public class Trigonometry {
+
+    private String funkcja;
+
+    public Trigonometry() {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Podaj funkcje trygonometryczna (sin/cos/tan/ctg): ");
+        this.funkcja = scanner.nextLine();
+
+        if(this.funkcja != "sin" || this.funkcja != "cos" || this.funkcja != "tan" || this.funkcja != "ctg") {
+            this.funkcja = "sin";
+        }
+    }
+
+    public String getFunkcja() {
+        return funkcja;
+    }
+}
