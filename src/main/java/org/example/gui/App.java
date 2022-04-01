d0a9438fecf844aa41bbb349911822ca1960d966
Damian2057 <ddamian2057@gmail.com>
2022-04-01 10:14:49 +0200

    Importing a class for drawing charts, creating a node class

--- src/main/java/org/example/gui/App.java
+++ src/main/java/org/example/gui/App.java
@@ -9,6 +9,11 @@ public class App {
 
     public static void main(String[] args) {
 
+        double minRange = 0;
+        double maxRange = 0;
+        int numberOfNodes = 0;
+        Scanner scanner= new Scanner(System.in);
+
         System.out.println("Lagrange interpolation methods on Chebyshev nodes\n");
 
         FunctionContainer functionContainer = null;
@@ -20,8 +25,14 @@ public class App {
                 System.out.println(e.getMessage());
             }
         }
+        System.out.println("Enter the lower range:");
+        minRange = Double.parseDouble(scanner.nextLine());
 
+        System.out.println("Enter the upper range:");
+        minRange = Double.parseDouble(scanner.nextLine());
 
+        System.out.println("Enter the number of interpolation nodes :");
+        numberOfNodes = Integer.parseInt(scanner.nextLine());
 
 
     }
