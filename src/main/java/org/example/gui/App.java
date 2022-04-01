15890971c33f706ddc58c8cc800359c4caa206c9
Damian2057 <ddamian2057@gmail.com>
2022-04-01 10:39:36 +0200

    Implementation of mechanisms of other classes

--- src/main/java/org/example/gui/App.java
+++ src/main/java/org/example/gui/App.java
@@ -2,8 +2,6 @@ package org.example.gui;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import org.example.exception.DataException;
 import org.example.model.*;
 
 public class App {
@@ -41,7 +39,28 @@ public class App {
                 = new ChebyshevInterpolation(functionContainer);
         chebyshevInterpolation.initializeNodes(minRange,maxRange,numberOfNodes,nodes);
 
-        System.out.println(nodes.size());
+        ArrayList<Double> originXFunction = new ArrayList<>();
+        ArrayList<Double> originYFunction = new ArrayList<>();
+        ArrayList<Double> interpolatedXFunction = new ArrayList<>();
+        ArrayList<Double> interpolateYFunction = new ArrayList<>();
+
+        double accuracyLeap = 0.001;
+        double temp = minRange; //started X on the chart
+
+        while (temp <= maxRange) {
+            originXFunction.add(temp);
+            originYFunction.add(functionContainer.function(temp));
+
+            interpolatedXFunction.add(temp);
+            //interpolateYFunction.add();
+
+            temp += accuracyLeap;
+        }
+
+//        XYSeriesDemo xySeriesDemo = new XYSeriesDemo("title",originXFunction,originYFunction);
+//        xySeriesDemo.pack();
+//        xySeriesDemo.setVisible(true);
+
     }
 }
 
